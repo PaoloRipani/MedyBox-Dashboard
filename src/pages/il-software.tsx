1dfc2f7192e465662b4b7db05c79ee901382f733
PaoloRipani <ripani.1823210@studenti.uniroma1.it>
2024-03-28 16:46:37 +0100

    0.2.1

---
+++ src/pages/il-software.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Layout from '../app/layout'
+import '../app/globals.css'
+import '../app/globals.scss'
+
+
+export default function IlSoftware() {
+
+
+return (
+    <>
+    <Layout>
+        <>
+            {/* First Section - Scopri il software */}
+            {/* 3 Elementi in verticale */}
+
+            {/* Second Section - i vantaggi di Gesiqa */}
+            {/* 3 Elementi in verticale. L'ultimo ha una griglia a 4 colonne che diventa 
+            a 2 colonne in mobile*/}
+
+            {/* Third Section - funzionalità principali */}
+            {/* 3 Elementi in verticale */}
+
+            {/* Fourth Section - vuoi approfondire */}
+            {/* 2 Elementi in verticale */}
+
+            {/* Fifth Section - scopri i moduli */}
+        </>
+    </Layout>
+    </>
+)}
